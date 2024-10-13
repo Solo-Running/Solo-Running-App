f676530174bb6f8f7d46105c604a9bff0c8a2899
William Kim <wik002@ucsd.edu>
2024-10-13 16:46:19 -0700

    Initial Commit

---
+++ Solo/SoloApp.swift
@@ -0,0 +1,32 @@
+//
+//  SoloApp.swift
+//  Solo
+//
+//  Created by William Kim on 10/13/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SoloApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
