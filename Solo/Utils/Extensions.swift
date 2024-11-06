39ae8d3ee0d605c10c99d3f601d76b53ff2a06d1
William Kim <wik002@ucsd.edu>
2024-11-06 11:07:21 -0800

    moved debouncer class here for now

--- Solo/Utils/Extensions.swift
+++ Solo/Utils/Extensions.swift
@@ -20,3 +20,24 @@ extension Color {
     }
 }
 
+
+class Debouncer {
+    private let delay: TimeInterval
+    private var workItem: DispatchWorkItem?
+    private let queue: DispatchQueue
+   
+ init(delay: TimeInterval, queue: DispatchQueue = DispatchQueue.main) {
+        self.delay = delay
+        self.queue = queue
+    }
+ func debounce(action: @escaping (() -> Void)) {
+        workItem?.cancel()
+        workItem = DispatchWorkItem { [weak self] in
+            action()
+            self?.workItem = nil
+        }
+        if let workItem = workItem {
+            queue.asyncAfter(deadline: .now() + delay, execute: workItem)
+        }
+    }
+}
