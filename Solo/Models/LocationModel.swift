597929b3f4bc6ff48d711db82901e0b2166feaed
William Kim <wik002@ucsd.edu>
2024-11-08 10:00:10 -0800

    created new model for saving locations in swiftdata

--- Solo/Models/LocationModel.swift
+++ Solo/Models/LocationModel.swift
@@ -6,3 +6,114 @@
 //
 
 import Foundation
+import SwiftData
+import SwiftUI
+import MapKit
+
+
+
+
+// MTPlacemark is used for encoding any location into a consistent datat structure for displaying placemarks on the map
+@Model
+class MTPlacemark: Identifiable {
+    
+    @Attribute(.unique) var id: String = UUID().uuidString
+    var name: String?
+    var thoroughfare: String?
+    var subThoroughfare: String?
+    var locality: String?
+    var subLocality: String?
+    var administrativeArea: String?
+    var subAdministrativeArea: String?
+    var postalCode: String?
+    var country: String?
+    var isoCountryCode: String?
+    var longitude: Double
+    var latitude: Double
+    var isCustomLocation: Bool
+    
+    init(id: String = UUID().uuidString,
+         name: String? = "",
+         thoroughfare: String? = "",
+         subThoroughfare: String? = "",
+         locality: String? = "",
+         subLocality: String? = "",
+         administrativeArea: String? = "",
+         subAdministrativeArea: String? = "",
+         postalCode: String? = "",
+         country: String? = "",
+         isoCountryCode: String? = "",
+         longitude: Double = 0.0,
+         latitude: Double = 0.0,
+         isCustomLocation: Bool = false
+    ) {
+         
+        self.id = id
+        self.name = name
+        self.thoroughfare = thoroughfare
+        self.subThoroughfare = subThoroughfare
+        self.locality = locality
+        self.subLocality = subLocality
+        self.administrativeArea = administrativeArea
+        self.subAdministrativeArea = subAdministrativeArea
+        self.postalCode = postalCode
+        self.country = country
+        self.isoCountryCode = isoCountryCode
+        self.longitude = longitude
+        self.latitude = latitude
+        self.isCustomLocation = isCustomLocation
+    }
+    
+    // Method to get CLLocationCoordinate2D for start placemark
+    func getLocation() -> CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: self.latitude, longitude: self.longitude)
+    }
+}
+
+
+@Model
+class Location {
+    @Attribute(.unique) var id: String = UUID().uuidString
+    var name: String?
+    var thoroughfare: String?
+    var subThoroughfare: String?
+    var locality: String?
+    var subLocality: String?
+    var administrativeArea: String?
+    var subAdministrativeArea: String?
+    var postalCode: String?
+    var country: String?
+    var isoCountryCode: String?
+    var latitude: Double
+    var longitude: Double
+    
+    init(id: String = UUID().uuidString,
+         name: String? = "",
+         thoroughfare: String? = "",
+         subThoroughfare: String? = "",
+         locality: String? = "",
+         subLocality: String? = "",
+         administrativeArea: String? = "",
+         subAdministrativeArea: String? = "",
+         postalCode: String? = "",
+         country: String? = "",
+         isoCountryCode: String? = "",
+         longitude: Double,
+         latitude: Double
+    ) {
+         
+        self.id = id
+        self.name = name
+        self.thoroughfare = thoroughfare
+        self.subThoroughfare = subThoroughfare
+        self.locality = locality
+        self.subLocality = subLocality
+        self.administrativeArea = administrativeArea
+        self.subAdministrativeArea = subAdministrativeArea
+        self.postalCode = postalCode
+        self.country = country
+        self.isoCountryCode = isoCountryCode
+        self.longitude = longitude
+        self.latitude = latitude
+    }
+}
