8cd0ae728a402f2a81e386ac03b76fab1d9377cf
William Kim <wik002@ucsd.edu>
2025-02-17 00:08:32 -0800

    moved files to new group

---
+++ Solo/Screens/Main/MainView.swift
@@ -0,0 +1,147 @@
+//
+//  MainView.swift
+//  Solo
+//
+//  Created by William Kim on 10/14/24.
+//
+
+import Foundation
+import SwiftUI
+import SwiftData
+import StoreKit
+
+
+/**
+ The primary access point  to the app that handles overhead logic for utility managers and background tasks
+ including subscription status tasks, user navigation, user profiile, and the app state.
+ */
+struct MainView: View {
+    
+    @Environment(AppState.self) var appState
+    @Environment(\.modelContext) private var modelContext
+
+    @EnvironmentObject var locationManager: LocationManager
+    @EnvironmentObject var activityManager: ActivityManager
+    @EnvironmentObject var subscriptionManager: SubscriptionManager
+
+    @State var selectedScreen: Screen = .Dashboard
+    @State var oldSelectedScreen: Screen = .Dashboard
+    @State private var showRunView: Bool = false  // Control visibility of RunView
+    @State private var permissionsSheetDetents: Set<PresentationDetent> =  [.large]
+    
+    @Query var userData: [UserModel]
+    var user: UserModel? {userData.first}
+    
+    
+    var body: some View {
+        
+        ZStack {
+            if !subscriptionManager.isSubscribed  {
+                // This view also handles susbcription management
+                OnboardingView()
+            }
+            else if user == nil {
+                ProvideCredentialsView()
+                    .environment(appState)
+            }
+            else {
+                
+                ZStack(alignment: .bottom) {
+                   
+                    TabView(selection: Binding(get: { appState.screen }, set: { newScreen in
+                        appState.screen = newScreen
+                        showRunView = newScreen == .Run
+                    })){
+                        
+                        Group {
+                            DashboardView().tabItem {
+                                Label("Home",systemImage: "house.fill")
+                            }
+                            .tag(Screen.Dashboard)
+                            
+                            // Attach an empty view since the Run Screen appears as a full cover on top
+                            VStack{}.background(.black)
+                                .tabItem {
+                                    Label("Add Run",systemImage: "plus.circle.fill")
+                                }
+                                .tag(Screen.Run)
+                            
+                            ProfileView().tabItem {
+                                Label("Profile",systemImage: "person.circle.fill")
+                            }
+                            .tag(Screen.Profile)
+                        }
+                        .toolbarColorScheme(.dark, for: .tabBar)
+                        .toolbarBackground(.black, for: .tabBar)
+                    }
+                    .sensoryFeedback(.impact, trigger: appState.screen)
+                  
+                    .tint(.white)
+                    .onChange(of: appState.screen, initial: false) { old, new in
+                        // Try to remember what screen we were looking at previously
+                        if appState.screen == .Run {
+                            appState.screen = old
+                        }
+                    }
+                    .fullScreenCover(isPresented: $showRunView, onDismiss: {
+                        self.selectedScreen = self.oldSelectedScreen
+                    }) {
+                        
+                        if !activityManager.isMotionAuthorized || !locationManager.isAuthorized {
+                            VStack(alignment: .leading) {
+                                
+                                Button {
+                                    showRunView = false
+                                } label: {
+                                    ZStack {
+                                        Circle()
+                                            .frame(width: 32, height: 32)
+                                            .foregroundStyle(.white)
+                                        Image(systemName: "chevron.down")
+                                            .foregroundStyle(.black)
+                                            .frame(width: 16, height: 16)
+                                            .fontWeight(.semibold)
+                                    }
+                                }
+                                .padding([.leading], 16)
+                                
+                                PermissionsView()
+                            }
+                            .background(DARK_GREY)
+                        }
+                        else {
+                            RunView(showRunView: $showRunView)
+                        }
+                    }
+                }
+            }
+        }
+        .subscriptionStatusTask(for: "21636260") { taskState in
+            print("Checking status task")
+            if let value = taskState.value {
+                print("Task state: \(taskState.value as Any)")
+                subscriptionManager.isSubscribed = !value
+                    .filter { $0.state != .revoked && $0.state != .expired  }
+                    .isEmpty
+            } else {
+                subscriptionManager.isSubscribed = false
+            }
+        }
+        .task {
+            await subscriptionManager.listenForTransactions()
+        }
+        .onAppear {
+            if !userData.isEmpty {
+                print("user info is not empty: \(userData)")
+                appState.hasCredentials = true
+                appState.screen = .Dashboard
+            }
+            if userData.isEmpty {
+                print("user data is empty")
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+    
+}
+
---
+++ Solo/Screens/Main/OnboardingView.swift
@@ -0,0 +1,177 @@
+//
+//  Onboarding.swift
+//  Solo
+//
+//  Created by William Kim on 1/27/25.
+//
+
+import Foundation
+import SwiftUI
+import StoreKit
+import AlertToast
+
+
+enum SelectedTab: Int {
+    case Welcome = 0, Roam, Data, Spotify, Start
+}
+
+struct OnboardingView: View {
+    @EnvironmentObject var subscriptionManager: SubscriptionManager
+
+    @State private var isSubscribed: Bool = false
+    @State var selectedTab: SelectedTab = .Welcome
+
+    @AppStorage("ignoreOnboarding") var ignoreOnboarding: Bool = false
+    @State private var showIgnoreToast: Bool = false
+  
+    var body: some View {
+        NavigationStack {
+            VStack {
+                TabView {
+                    Group {
+                        VStack(spacing: 8) {
+                            Image("SoloLogo")
+                                .tint(.white)
+                                .frame(width: 64, height: 64)
+                                .padding(.bottom, 16)
+
+                            Text("Welcome")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                                .foregroundStyle(.white)
+                            
+                            Text("Solo Running is an app that helps users track and record runs on the go.")
+                                .foregroundStyle(TEXT_LIGHT_GREY)
+                                .multilineTextAlignment(.center)
+                        }
+                        .tag(SelectedTab.Welcome)
+                        
+                        VStack(spacing: 8)  {
+                            Image(systemName: "mappin.circle.fill")
+                                .tint(.white)
+                                .font(.system(size: 64))
+                                .padding(.bottom, 16)
+
+                            Text("Roam Anywhere")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                                .foregroundStyle(.white)
+                            
+                            Text("Leverage an interactive map to find your favorite locations or save them for later!")
+                                .foregroundStyle(TEXT_LIGHT_GREY)
+                                .multilineTextAlignment(.center)
+                        }
+                        .tag(SelectedTab.Roam)
+
+                        
+                        VStack(spacing: 8)  {
+                            Image(systemName: "checkmark.seal.fill")
+                                .tint(.white)
+                                .font(.system(size: 64))
+                                .padding(.bottom, 16)
+
+                            
+                            Text("Secure Data")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                                .foregroundStyle(.white)
+                            
+                            Text("All your information is stored locally on device and naver shared with anyone.")
+                                .foregroundStyle(TEXT_LIGHT_GREY)
+                                .multilineTextAlignment(.center)
+                            
+                        }
+                        .tag(SelectedTab.Data)
+
+                        
+                        VStack(spacing: 8)  {
+                            Image(systemName: "music.note")
+                                .tint(.white)
+                                .font(.system(size: 64))
+                                .padding(.bottom, 16)
+
+                            Text("Music In Your Hands")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                                .foregroundStyle(.white)
+                            
+                            Text("Play Spotify directly from in the app while running!")
+                                .foregroundStyle(TEXT_LIGHT_GREY)
+                                .multilineTextAlignment(.center)
+                            
+                        }
+                        .tag(SelectedTab.Spotify)
+                        
+                        
+                        VStack(spacing: 8)  {
+                            
+                            Image(systemName: "figure.run")
+                                .tint(.white)
+                                .font(.system(size: 64))
+                                .padding(.bottom, 16)
+                            
+                            
+                            Text("Get Started Today")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                                .foregroundStyle(.white)
+                            
+                            Text("An active trial or subscription is required to use the app.")
+                                .foregroundStyle(TEXT_LIGHT_GREY)
+                                .multilineTextAlignment(.center)
+
+                            
+                            Spacer().frame(height: 32)
+                            
+                            NavigationLink {
+                                // if user has subscription, go to dashboard
+                                if isSubscribed {
+                                    DashboardView()
+                                } else {
+                                    SubscriptionLaunchView()
+                                }
+                            } label : {
+                                HStack {
+                                    Text("Continue").foregroundStyle(.white)
+                                }
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(BLUE)
+                                .cornerRadius(12)
+                            }
+                            
+                        }
+                        .tag(SelectedTab.Start)
+
+                        
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(24)
+                }
+                .tabViewStyle(.page(indexDisplayMode: .always))
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+                .frame(maxHeight: .infinity)
+            }
+            .toast(isPresenting: $showIgnoreToast, tapToDismiss: true) {
+                AlertToast(type: .systemImage("checkmark.circle", Color.green), title: "You will no longer see onboarding content next time.")
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink {
+                        if isSubscribed {
+                            DashboardView()
+                        } else {
+                            SubscriptionLaunchView()
+                        }
+                    } label : {
+                        Text("Skip")
+                            .foregroundStyle(TEXT_LIGHT_GREY)
+                    }
+                }
+            }
+            .toolbarBackground(.black, for: .navigationBar)
+            .preferredColorScheme(.dark)
+        }
+    }
+}
+
