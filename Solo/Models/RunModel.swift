f4361bfa5817ca987de66e8260255e501fe34353
William Kim <wik002@ucsd.edu>
2025-02-25 18:01:32 -0800

    added route distance to model

--- Solo/Models/RunModel.swift
+++ Solo/Models/RunModel.swift
@@ -22,6 +22,7 @@ final class Run: Identifiable{
     var endTime: Date = Date()
     var elapsedTime: Int  = 0               // seconds
     var distanceTraveled: Double  = 0.0     // distance in meters
+    var routeDistance: Double = 0.0
     var steps: Int = 0
     var startPlacemark: MTPlacemark?
     @Relationship(deleteRule: .noAction) var endPlacemark: MTPlacemark? // don't delete the endPlacemark. The user should do this manually
@@ -30,7 +31,7 @@ final class Run: Identifiable{
     @Attribute(.externalStorage) var routeImage: Data?
     var notes: String = ""
     
-    init(id: String = UUID().uuidString, isDarkMode: Bool = false, postedDate:Date, startTime: Date, endTime: Date, elapsedTime: Int, distanceTraveled: Double, steps: Int, startPlacemark: MTPlacemark, endPlacemark: MTPlacemark,  avgSpeed: Double, avgPace: Int, routeImage: Data, notes: String = "") {
+    init(id: String = UUID().uuidString, isDarkMode: Bool, postedDate:Date, startTime: Date, endTime: Date, elapsedTime: Int, distanceTraveled: Double, routeDistance: Double, steps: Int, startPlacemark: MTPlacemark, endPlacemark: MTPlacemark,  avgSpeed: Double, avgPace: Int, routeImage: Data, notes: String = "") {
         self.id = id
         self.isDarkMode = isDarkMode
         self.postedDate = postedDate
@@ -39,6 +40,7 @@ final class Run: Identifiable{
         self.elapsedTime = elapsedTime
         self.steps = steps
         self.distanceTraveled = distanceTraveled
+        self.routeDistance = routeDistance
         self.startPlacemark = startPlacemark
         self.endPlacemark = endPlacemark
         self.avgSpeed = avgSpeed
