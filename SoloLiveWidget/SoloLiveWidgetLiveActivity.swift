0e6546cca525b105fb8cbcb491ba1df3166b8448
William Kim <wik002@ucsd.edu>
2025-02-25 18:04:44 -0800

    tweaking live widget for island

--- SoloLiveWidget/SoloLiveWidgetLiveActivity.swift
+++ SoloLiveWidget/SoloLiveWidgetLiveActivity.swift
@@ -33,10 +33,10 @@ struct SoloLiveWidgetAttributes: ActivityAttributes {
 }
 
 func formattedSteps(_ steps: Int) -> String {
-    if steps >= 1_000_000 {
-        return "\(steps / 1_000_000)M"
-    } else if steps >= 1_000 {
-        let formatted = Double(steps) / 1_000
+    if steps >= 1000000 {
+        return "\(steps / 1000000)M"
+    } else if steps >= 1000 {
+        let formatted = Double(steps) / 1000
         return String(format: "%.2fk", formatted).replacingOccurrences(of: ".00", with: "")
     } else {
         return "\(steps)"
@@ -113,36 +113,58 @@ struct SoloLiveWidgetLiveActivity: Widget {
             
             .activitySystemActionForegroundColor(.white)
         } dynamicIsland: { context in
-              // Create the presentations that appear in the Dynamic Island.
-              DynamicIsland {
-                  
-                  DynamicIslandExpandedRegion(.leading) {
-                     Image(systemName: "figure.walk.motion")
-                          .foregroundStyle(.black)
-                       .font(.title2)
-                   }
-                  
-                    DynamicIslandExpandedRegion(.trailing) {
-                      Label {
-                          Text("\(context.state.secondsElapsed)")
-                            .multilineTextAlignment(.trailing)
-                            .frame(width: 50)
-                            .monospacedDigit()
-                      } icon: {
-                          Image(systemName: "timer")
-                              .foregroundColor(.indigo)
-                      }
-                      .font(.title2)
+            DynamicIsland {
+                    // Expanded Regions
+                    DynamicIslandExpandedRegion(.leading) {
+                        EmptyView() // No visible content
                     }
-              } compactLeading: {
-                  // Create the compact leading presentation.
-                  Text(context.attributes.timerName)
-              } compactTrailing: {
-                  // Create the compact trailing presentation.
-                  Text("\(context.state.secondsElapsed)")
-              } minimal: {
-                  Text("\(context.state.secondsElapsed)")
-              }
+                    DynamicIslandExpandedRegion(.trailing) {
+                        EmptyView() // No visible content
+                    }
+                    DynamicIslandExpandedRegion(.bottom) {
+                        EmptyView() // No visible content
+                    }
+                } compactLeading: {
+                    // Compact leading view content
+                    Text("Compact")
+                } compactTrailing: {
+                    // Compact trailing view content
+                    Text("View")
+                } minimal: {
+                    // Minimal view content
+                    Text("Min")
+                }
+                .keylineTint(Color.clear)
+              // Create the presentations that appear in the Dynamic Island.
+//              DynamicIsland {
+//                  
+//                  DynamicIslandExpandedRegion(.leading) {
+//                     Image(systemName: "figure.walk.motion")
+//                          .foregroundStyle(.black)
+//                       .font(.title2)
+//                   }
+//                  
+//                    DynamicIslandExpandedRegion(.trailing) {
+//                      Label {
+//                          Text("\(context.state.secondsElapsed)")
+//                            .multilineTextAlignment(.trailing)
+//                            .frame(width: 50)
+//                            .monospacedDigit()
+//                      } icon: {
+//                          Image(systemName: "timer")
+//                              .foregroundColor(.indigo)
+//                      }
+//                      .font(.title2)
+//                    }
+//              } compactLeading: {
+//                  // Create the compact leading presentation.
+//                  Text(context.attributes.timerName)
+//              } compactTrailing: {
+//                  // Create the compact trailing presentation.
+//                  Text("\(context.state.secondsElapsed)")
+//              } minimal: {
+//                  Text("\(context.state.secondsElapsed)")
+//              }
           }
     }
 }
