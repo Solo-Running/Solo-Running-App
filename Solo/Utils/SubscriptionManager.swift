83aa1b68f5f49275fc947098930eb0118c4b1888
William Kim <wik002@ucsd.edu>
2025-03-02 19:31:38 -0800

    updated logic to check subscription expiratio status

--- Solo/Utils/SubscriptionManager.swift
+++ Solo/Utils/SubscriptionManager.swift
@@ -14,6 +14,34 @@ import StoreKit
  https://medium.com/@mafz/how-to-enable-sandbox-in-app-purchases-for-ios-apps-71215c250595
  */
 
+struct Payload: Codable {
+    let bundleId: String
+    let currency: String
+    let deviceVerification: String
+    let deviceVerificationNonce: String
+    let environment: String
+    let expiresDate: Int
+    let inAppOwnershipType: String
+    let isUpgraded: Bool?
+    let offerDiscountType: String?
+    let offerType: Int?
+    let originalPurchaseDate: Int
+    let originalTransactionId: String
+    let price: Int
+    let productId: String
+    let purchaseDate: Int
+    let quantity: Int
+    let signedDate: Int
+    let storefront: String
+    let storefrontId: String
+    let subscriptionGroupIdentifier: String
+    let transactionId: String
+    let transactionReason: String
+    let type: String
+    let webOrderLineItemId: String
+}
+
+
 
 @MainActor
 final class SubscriptionManager: NSObject, ObservableObject {
@@ -23,11 +51,11 @@ final class SubscriptionManager: NSObject, ObservableObject {
     
     @Published var isSubscribed: Bool = false
     
-    @Published var currentTransaction: Transaction?
+    @Published var currentEntitlement: Transaction?
     @Published var transactionPayload: Data?                                    // used to get the price and offer discount type information
     @Published var currentRenewalInfo: Product.SubscriptionInfo.RenewalInfo?    // used to determine if the current subscription will expire or renew
-
     @Published var recentPurchases: [Transaction] = []
+    
     @Published var isLoadingPurchases: Bool = true
     @Published var isLoadingEntitlement: Bool = true
     
@@ -37,7 +65,29 @@ final class SubscriptionManager: NSObject, ObservableObject {
             await getSubscriptionStatusAndEntitlement()
         }
     }
+    
+    func clearEntitlementData() {
+        currentEntitlement = nil
+    }
 
+    func hasSubscriptionExpired() -> Bool {
+        // If the user has no active entitlements, then they have no susbcription
+        guard let entitlement = currentEntitlement else { return true}
+        guard let renewalInfo = currentRenewalInfo else { return true}
+        
+        //print("Entitlement Expiration Date: \( String(describing: entitlement.expirationDate)) VS NOW Date: \(Date())")
+        // print("Will sub auto renew: \(renewalInfo.willAutoRenew)")
+        
+        // Dont assume that that an empty expiration date means the subscription is still active
+        guard let expirationDate = entitlement.expirationDate else { return true }
+        
+        let hasExpired = expirationDate < Date() && !renewalInfo.willAutoRenew
+        // print("Has susbcription expired: \(hasExpired)")
+        
+        return hasExpired
+    }
+    
+    
     // Listen for storekit updates
     func listenForTransactions() async {
         for await verificationResult in Transaction.updates {
@@ -57,7 +107,6 @@ final class SubscriptionManager: NSObject, ObservableObject {
         
        case .none:
            print("Transaction successful: \(transaction.id)")
-                
            // Finish transaction to remove it from queue
            await transaction.finish()
        }
@@ -95,12 +144,6 @@ final class SubscriptionManager: NSObject, ObservableObject {
                 continue
             }
                         
-            // Get the current subscription data
-//            if productIds.contains(transaction.productID) {
-//                currentTransaction = transaction
-//                transactionPayload = result.payloadData
-//            }
-//            
             // Get the renewal information for the current subscription
             do {
                 let products = try await Product.products(for: [transaction.productID])
@@ -109,20 +152,22 @@ final class SubscriptionManager: NSObject, ObservableObject {
                         let productSubInfo = try await product.subscription?.status
                         
                         for subInfo in productSubInfo! {
-                            if subInfo.state == .subscribed {
-                                
-                                currentTransaction = transaction
-                                transactionPayload = result.payloadData
-                                
+                            if subInfo.state == .subscribed && subInfo.state != .expired {
+                                // print("SUB INFO: \(subInfo)")
                                 switch subInfo.renewalInfo {
                                 case .verified(let prodStatus):
                                     if (prodStatus.currentProductID == transaction.productID) {
+                                        currentEntitlement = transaction
+                                        transactionPayload = result.payloadData
                                         currentRenewalInfo = prodStatus
                                     }
                                     break
                                 default:
                                     break
-                                }}
+                                }
+                            } else {
+                                currentEntitlement = nil
+                            }
                         }
                     } catch {
                         print(error.localizedDescription)
