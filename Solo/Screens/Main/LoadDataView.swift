76d89c7ece998b1f3ca7e3a467ba80d080dba6b9
William Kim <wik002@ucsd.edu>
2025-03-15 17:07:39 -0700

    added a experimental view to show loading icloud data screen

--- Solo/Screens/Main/LoadDataView.swift
+++ Solo/Screens/Main/LoadDataView.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import SwiftUI
+
+
+struct LoadDataView: View {
+    var body: some View {
+        VStack(alignment: .center) {
+            ProgressView()
+            
+            Text("Loading your iCloud data. Please wait.")
+                .multilineTextAlignment(.center)
+                .frame(maxWidth: 200)
+                .padding(.top, 8)
+                .foregroundStyle(TEXT_LIGHT_GREY)
+        }
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+    }
+}
