3b8d99b99750a0898c9a3a5b3b24ce496a8b3ff8
William Kim <wik002@ucsd.edu>
2025-03-15 17:07:12 -0700

    modified app state variables for loading icloud data. Not necessary atm

--- Solo/AppState.swift
+++ Solo/AppState.swift
@@ -12,12 +12,18 @@ enum Screen: Int {
     case Dashboard = 0, Run, Profile
 }
 
+enum TimePeriod: Int {
+    case Week = 0, Month, Year
+}
+
 @Observable
 final class AppState {
     var hasCredentials: Bool
     var screen: Screen  = .Dashboard
+    var isLoadingCloudData: Bool
     
     init() {
         hasCredentials = false
+        isLoadingCloudData = false
     }
 }
