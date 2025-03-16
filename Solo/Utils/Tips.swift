c74c7d4573b587fd5f8f9bd4a8a370a8e4b83354
William Kim <wik002@ucsd.edu>
2025-03-15 17:04:41 -0700

    added a tip for the dashboard charts

--- Solo/Utils/Tips.swift
+++ Solo/Utils/Tips.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import TipKit
+import SwiftUI
+
+struct DashboardChartTip: Tip {
+    var title: Text {
+        Text("Weekly Data")
+    }
+    
+    var message: Text? {
+        Text("Swipe horizontally to see your steps and time over the week")
+    }
+    
+    var image: Image? {
+        Image(systemName: "hand.draw")
+    }
+}
