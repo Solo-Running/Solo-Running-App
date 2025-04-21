cd38b2edfc517b5c86b39997f406a990b8fb7aaa
William Kim <wik002@ucsd.edu>
2025-04-20 18:17:05 -0700

    added look around manager

--- Solo/Utils/LookAroundManager.swift
+++ Solo/Utils/LookAroundManager.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import SwiftUI
+import MapKit
+
+class LookAroundManager: ObservableObject {
+
+    var lookAroundScene: MKLookAroundScene?
+    var coordinate: CLLocationCoordinate2D?
+    
+    func resetData() {
+        lookAroundScene = nil
+        coordinate = nil
+    }
+    
+    func loadPreview() async {
+        Task {
+            if let coordinate = coordinate {
+                let request = MKLookAroundSceneRequest(coordinate: coordinate)
+                do {
+                    lookAroundScene = try await request.scene
+                } catch (let error) {
+                    print(error)
+                }
+            }
+        }
+    }
+    
+}
